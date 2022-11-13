fbda939aeff1638ab92d08007f56c3e13727ef4c
ndillon <vycheslav79@gmail.com>
2022-11-13 18:43:44 +0300

    asd

--- srcs/textures.c
+++ srcs/textures.c
@@ -6,7 +6,7 @@
 /*   By: fate <fate@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 12:10:33 by gtrinida          #+#    #+#             */
-/*   Updated: 2022/11/13 18:36:26 by fate             ###   ########.fr       */
+/*   Updated: 2022/11/13 18:43:20 by fate             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ t_texture	*xpg_to_img(t_mlx *mlx, char *path_to_file)
 	t->img = mlx_xpm_file_to_image(mlx->mlx_ptr,
 			path_to_file, &(t->width), &(t->height));
 	if (!t->img)
+	{
+		free(t);
 		return (NULL);
+	}
 	t->texture = (int *)mlx_get_data_addr(t->img, &(t->bpp),
 			&(t->size_line), &(t->endian));
 	return (t);
